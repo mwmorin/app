bc765efe4a1029e3045e821d178830b64452a8f5
Michael Morin <mwmorin@yahoo.com>
2024-01-10 17:37:31 +0000

    added allowedHeaders to enable POST from cross origin

--- src/main/java/com/mwmorin/wordlesolver/controller/WordleSolverRestController.java
+++ src/main/java/com/mwmorin/wordlesolver/controller/WordleSolverRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class WordleSolverRestController {
 
